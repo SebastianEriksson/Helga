0460f0d5699a5544f9444a7f044d2a13df74c5d2
SebastianEriksson <erikssonsebastian@outlook.com>
2016-12-07 14:16:17 +0200

    Finish Members.class
    - Add getters and setters
    - Add toString
    - Link Position

--- src/main/java/fi/haagahelia/course/domain/Member.java
+++ src/main/java/fi/haagahelia/course/domain/Member.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+// Define Member entity
 @Entity
 public class Member {
 	@Id
@@ -20,9 +21,77 @@ public class Member {
 	private double valid;
 	private String membership;
 	
+	// Insert position
 	@ManyToOne
 	@JsonIgnore
 	@JoinColumn(name = "positionid")
 	private Position position;
 	
+	// Getters and setters
+	public Member() {}
+	
+	public Member(String firstName, String surname, String email, double valid, 
+			String membership) {
+		this.firstName = firstName;
+		this.surname = surname;
+		this.email = email;
+		this.valid = valid;
+		this.membership = membership;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public double getValid() {
+		return valid;
+	}
+	public void setValid(double valid) {
+		this.valid = valid;
+	}
+	public String getMembership() {
+		return membership;
+	}
+	public void setMembership(String membership) {
+		this.membership = membership;
+	}
+	
+	public Position getPosition() {
+		return position;
+	}
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+	
+	@Override
+	public String toString() {
+		if (this.position != null)
+			return "Member [id=" + id + ", first name=" + firstName + ", surname=" + 
+					surname + ", email=" + email + ", valid to=" + valid +
+					", membership=" + membership + ", position=" + this.getPosition() + "]";
+		else
+			return "Member [id=" + id + ", first name=" + firstName + ", surname="+
+					surname + ", email=" + email + ", valid to=" + valid +
+					", membership=" + membership + "]";
+	}
 }

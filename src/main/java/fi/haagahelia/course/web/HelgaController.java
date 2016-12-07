3f08cf85d48d181401455ad68d8ff16b98ac7854
SebastianEriksson <erikssonsebastian@outlook.com>
2016-12-07 13:41:10 +0200

    Add comment

--- src/main/java/fi/haagahelia/course/web/HelgaController.java
+++ src/main/java/fi/haagahelia/course/web/HelgaController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelgaController {
 	
-	
+	// Takes user to login page when connecting
 	@RequestMapping(value ="/")
 	public String index() {
-		return "index";
+		return "login";
 	}
 }

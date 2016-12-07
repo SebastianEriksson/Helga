c10f06bd46af5d45e34efd0a9860826978338bd9
SebastianEriksson <erikssonsebastian@outlook.com>
2016-12-07 12:08:44 +0200

    First commit

---
+++ src/main/java/fi/haagahelia/course/HelgaApplication.java
@@ -0,0 +1,12 @@
+package fi.haagahelia.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelgaApplication.class, args);
+	}
+}

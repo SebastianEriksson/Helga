ab827d80c4e9e2b60462989b2549be90d39dac82
SebastianEriksson <erikssonsebastian@outlook.com>
2016-12-08 21:55:15 +0200

    Continue working on HelgaApplication
    - Add pre-made positions
    - Enable basic logging
    - Add deployment sections
    - Still needs pre-made users etc

--- src/main/java/fi/haagahelia/course/HelgaApplication.java
+++ src/main/java/fi/haagahelia/course/HelgaApplication.java
@@ -1,12 +1,56 @@
 package fi.haagahelia.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.course.domain.Member;
+import fi.haagahelia.course.domain.MemberRepository;
+import fi.haagahelia.course.domain.Position;
+import fi.haagahelia.course.domain.PositionRepository;
 
 @SpringBootApplication
-public class HelgaApplication {
-
+public class HelgaApplication extends SpringBootServletInitializer {
+	
+	// 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelgaApplication.class);
+	}
+	
+	// Part of the logging system
+	private static final Logger log = LoggerFactory.getLogger(HelgaApplication.class);
+	
+	// Main
 	public static void main(String[] args) {
 		SpringApplication.run(HelgaApplication.class, args);
+	}
+	
+	// Add pre-made positions, test users with login access
+	@Bean
+	public CommandLineRunner bookDemo(MemberRepository repository, PositionRepository prepository) {
+		return (args) -> {
+			log.info("Add a couple of book to the database");
+			
+			// Create pre-made positions
+			prepository.save(new Position("Member"));
+			prepository.save(new Position("Tutor"));
+			prepository.save(new Position("Headtutor"));
+			prepository.save(new Position("Helga"));
+			prepository.save(new Position("Admin"));
+			
+			// Create pre-made users
+			
+			// Show all the members in the log at startup
+			log.info("show all the members in the database");
+			for (Member member : repository.findAll()) {
+				log.info(member.toString());
+			}
+		};
 	}
 }

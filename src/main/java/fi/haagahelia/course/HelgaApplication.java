bdf6577603f6b19dd197383a7f97b03dbb7bb255
SebastianEriksson <erikssonsebastian@outlook.com>
2016-12-14 00:51:28 +0200

    Add more pre-made users

--- src/main/java/fi/haagahelia/course/HelgaApplication.java
+++ src/main/java/fi/haagahelia/course/HelgaApplication.java
@@ -48,21 +48,49 @@ public class HelgaApplication extends SpringBootServletInitializer {
 			
 			// Create pre-made users for members
 			User user1 = new User("kai", 
-					"$2a$10$pw6Z8aFUI6T94OauF08U7ubXlKK9yFubDj1jubtn4QuH3z1Epf8Hu", 
+					"$2a$10$52QRSr3MhTnsJilL/eghwOTXB4oYbFVhBeRxsnUCQ9Y8Er2qhCq92", 
 					"USER");
 			User user2 = new User("john", 
 					"$2a$10$HaTVlLziVQhbIh/ZKVrg1.LOZ7js07DGMhcjf1DHqCJN/Bo.03tZe", 
 					"ADMIN");
+			User user3 = new User("tommi", 
+					"$2a$10$52QRSr3MhTnsJilL/eghwOTXB4oYbFVhBeRxsnUCQ9Y8Er2qhCq92", 
+					"USER");
+			User user4 = new User("snowden", 
+					"$2a$10$52QRSr3MhTnsJilL/eghwOTXB4oYbFVhBeRxsnUCQ9Y8Er2qhCq92", 
+					"ADMIN");
+			User user5 = new User("sarah", 
+					"$2a$10$52QRSr3MhTnsJilL/eghwOTXB4oYbFVhBeRxsnUCQ9Y8Er2qhCq92", 
+					"USER");
+			User user6 = new User("timi", 
+					"$2a$10$52QRSr3MhTnsJilL/eghwOTXB4oYbFVhBeRxsnUCQ9Y8Er2qhCq92", 
+					"USER");
 			urepository.save(user1);
 			urepository.save(user2);
+			urepository.save(user3);
+			urepository.save(user4);
+			urepository.save(user5);
+			urepository.save(user6);
 			
 			// Create pre-made members
-			repository.save(new Member("John", "Snow", "johnsnow@got.hbo", "2017-12-12", 
-					mrepository.findByName("tutor").get(0), 
-					urepository.findByUsername("john")));
 			repository.save(new Member("Kai", "Mountain", "Kai@hotmail.com", "2017-12-12", 
 					mrepository.findByName("member").get(0), 
 					urepository.findByUsername("kai")));
+			repository.save(new Member("John", "Snow", "johnsnow@got.hbo", "2017-12-12", 
+					mrepository.findByName("headtutor").get(0), 
+					urepository.findByUsername("john")));
+			repository.save(new Member("Tommi", "Virtanen", "tommi@hotmail.com", "2017-12-14", 
+					mrepository.findByName("helga").get(0), 
+					urepository.findByUsername("tommi")));
+			repository.save(new Member("Snow", "Den", "snowden@hotmail.com", "2017-12-12", 
+					mrepository.findByName("admin").get(0), 
+					urepository.findByUsername("snowden")));
+			repository.save(new Member("Sarah", "Lind", "lind.sarah@gmail.com", "2017-12-12", 
+					mrepository.findByName("tutor").get(0), 
+					urepository.findByUsername("sarah")));
+			repository.save(new Member("Timi", "Koski", "koski@timi.com", "2017-12-12", 
+					mrepository.findByName("member").get(0), 
+					urepository.findByUsername("timi")));
 			
 			// Show all the members in the log at startup
 			log.info("show all the members in the database");
